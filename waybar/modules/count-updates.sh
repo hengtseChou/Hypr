abe963708b78c4411574b48cca03e03df1b9e386
hengtseChou <hankthedev@gmail.com>
2024-10-03 14:47:34 +0800

    fix: stop counting ignored pkg

--- waybar/modules/count-updates.sh
+++ waybar/modules/count-updates.sh
@@ -6,13 +6,6 @@
 #  \___/| .__/ \__,_|\__,_|\__\___||___/
 #       |_|
 #
-if ! updates_arch=$(checkupdates 2>/dev/null | wc -l); then
-    updates_arch=0
-fi
 
-if ! updates_aur=$(paru -Qua | wc -l); then
-    updates_aur=0
-fi
-
-updates=$(("$updates_arch" + "$updates_aur"))
+updates=$(paru -Qu | grep -v "\[ignored\]" | wc -l)
 printf '{"text": "%s", "alt": "%s", "tooltip": "%s Updates"}' "$updates" "$updates" "$updates"
