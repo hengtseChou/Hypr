7e5df37946d0dc8e691dcc472cb8cb8084d4e2a1
hengtseChou <hankthedev@gmail.com>
2024-10-09 14:36:40 +0800

    fix: count update using checkupdates pacman util

--- waybar/modules/count-updates.sh
+++ waybar/modules/count-updates.sh
@@ -7,5 +7,7 @@
 #       |_|
 #
 
-updates=$(paru -Qu | grep -v "\[ignored\]" | wc -l)
+updates_pacman=$(checkupdates | wc -l)
+updates_aur=$(paru -Qua | grep -v "\[ignored\]" | wc -l)
+updates=$((updates_pacman + updates_aur))
 printf '{"text": "%s", "alt": "%s", "tooltip": "%s Updates"}' "$updates" "$updates" "$updates"
